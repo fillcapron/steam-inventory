5e17c8fbde74a8519a989548134e0d9068bfe4d3
Alexander <spier1994@gmail.com>
2021-10-04 00:00:24 +0300

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Footer from './components/blocks/Footer';
 import Header from './components/blocks/Header';
-import Home from './pages/HomePage';
+import Home from './pages/Home';
 import ItemPage from './pages/ItemPage';
 import './scss/style.scss';
 
