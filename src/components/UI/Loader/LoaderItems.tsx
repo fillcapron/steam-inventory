52d99f2936973043065b9fa6161ba6bb9200fe57
Alexander <spier1994@gmail.com>
2021-10-03 23:28:24 +0300

    fix skeleton

--- src/components/UI/Loader/LoaderItems.tsx
+++ src/components/UI/Loader/LoaderItems.tsx
@@ -4,9 +4,9 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 const SkeletonItems: React.FC = () => {
     return (
         <>
-            <SkeletonTheme color="#8F98A0" highlightColor="#D2D2D2">
+            <SkeletonTheme color="#00000033" highlightColor="#8F98A0">
                 <div className="list-header">
-                    <Skeleton width={328} height={14} style={{marginLeft: "10px"}} />
+                    <Skeleton width={328} height={14} style={{ marginLeft: "10px" }} />
                     <div className="list-mode">
                         <MdViewHeadline className="list-mode-table" />
                         <MdViewModule className="list-mode-grid" />
@@ -17,15 +17,8 @@ const SkeletonItems: React.FC = () => {
                 Array(10)
                     .fill(0)
                     .map((_, index) => (
-                        <SkeletonTheme color="#8F98A0" highlightColor="#D2D2D2" key={index}>
-                            <div className="item-listing-row">
-                                <Skeleton height={32} width={32} className="item-listing-img" />
-                                <div className="item-listing-name-block">
-                                    <span className="item-listing-name"><Skeleton width={220} /></span>
-                                </div>
-                                <div className="item-listing-rarity"><Skeleton width={120} /></div>
-                                <div className="item-listing-count"><Skeleton width={80} /></div>
-                            </div>
+                        <SkeletonTheme color="#00000033" highlightColor="#8F98A0" key={index}>
+                            <Skeleton className="item-listing-row" />
                         </SkeletonTheme>
                     ))
             }
